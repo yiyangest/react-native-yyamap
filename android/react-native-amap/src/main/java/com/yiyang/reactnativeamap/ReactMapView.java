c76f241ddbce50415cc21586acb4125117c3acab
iDay <johnny.iday@gmail.com>
2016-07-24 11:55:23 +0800

    加入delta参数

--- android/react-native-amap/src/main/java/com/yiyang/reactnativeamap/ReactMapView.java
+++ android/react-native-amap/src/main/java/com/yiyang/reactnativeamap/ReactMapView.java
@@ -2,7 +2,14 @@ package com.yiyang.reactnativeamap;
 
 import android.content.Context;
 
+import com.amap.api.maps2d.AMap.OnCameraChangeListener;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.CameraPosition;
+import com.amap.api.maps2d.model.LatLng;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +17,7 @@ import java.util.List;
 /**
  * Created by yiyang on 16/2/29.
  */
-public class ReactMapView extends MapView {
+public class ReactMapView extends MapView implements OnCameraChangeListener {
     private List<ReactMapMarker> mMarkers = new ArrayList<ReactMapMarker>();
     private List<String> mMarkerIds = new ArrayList<String>();
 
@@ -19,6 +26,7 @@ public class ReactMapView extends MapView {
 
     public ReactMapView(Context context) {
         super(context);
+        this.getMap().setOnCameraChangeListener(this);
     }
 
     public void setOverlays(List<ReactMapOverlay> overlays) {
@@ -122,4 +130,81 @@ public class ReactMapView extends MapView {
 
         this.mMarkerIds = newMarkerIds;
     }
+    
+    public void onNativeEvent(String eventName, WritableMap eventData) {
+    	ReactContext reactContext = (ReactContext)getContext();
+        reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+            getId(),
+            eventName,
+            eventData);
+    }
+
+	@Override
+	public void onCameraChange(CameraPosition position) {
+		LatLng delta = zoomToDelta(position.zoom, position.target);
+		WritableMap eventData = Arguments.createMap();
+		WritableMap region = Arguments.createMap();
+		eventData.putBoolean("continuous", true);
+		eventData.putMap("region", region);
+		region.putDouble("latitude", position.target.latitude);
+		region.putDouble("longitude", position.target.longitude);
+		region.putDouble("latitudeDelta", delta.latitude);
+		region.putDouble("longitudeDelta", delta.longitude);
+		eventData.putMap("region", region);
+		this.onNativeEvent("topChange", eventData);
+	}
+
+	@Override
+	public void onCameraChangeFinish(CameraPosition position) {
+		LatLng delta = zoomToDelta(position.zoom, position.target);
+		WritableMap eventData = Arguments.createMap();
+		WritableMap region = Arguments.createMap();
+		eventData.putBoolean("continuous", false);
+		region.putDouble("latitude", position.target.latitude);
+		region.putDouble("longitude", position.target.longitude);
+		region.putDouble("latitudeDelta", delta.latitude);
+		region.putDouble("longitudeDelta", delta.longitude);
+		eventData.putMap("region", region);
+		this.onNativeEvent("topChange", eventData);
+	}
+
+	private LatLng zoomToDelta(float zoom, LatLng center) {
+		double centerPixelX = this.longitudeToPixelSpaceX(center.longitude);
+		double centerPixelY = this.latitudeToPixelSpaceY(center.latitude);
+		float zoomExponent = 20 - zoom;
+		double zoomScale = Math.pow(2, zoomExponent);
+		double scaledMapWidth = zoomScale * this.getWidth();
+		double scaledMapHeight = zoomScale * this.getHeight();
+		double topLeftPixelX = centerPixelX - (scaledMapWidth / 2);
+		double topLeftPixelY = centerPixelY - (scaledMapHeight / 2);
+		double minLng = this.pixelSpaceXToLongitude(topLeftPixelX);
+		double maxLng = this.pixelSpaceXToLongitude(topLeftPixelX + scaledMapWidth);
+		double longitudeDelta = maxLng - minLng;
+		double minLat = this.pixelSpaceYToLatitude(topLeftPixelY);
+		double maxLat = this.pixelSpaceYToLatitude(topLeftPixelY + scaledMapHeight);
+		double latitudeDelta = -1 * (maxLat - minLat);
+		return new LatLng(latitudeDelta, longitudeDelta);
+	}
+
+	private double longitudeToPixelSpaceX(double longitude) {
+		return Math.round(MERCATOR_OFFSET + MERCATOR_RADIUS * longitude * Math.PI / 180.0);
+	}
+
+	private double latitudeToPixelSpaceY(double latitude) {
+		return Math
+				.round(MERCATOR_OFFSET - MERCATOR_RADIUS
+						* Math.log(
+								(1 + Math.sin(latitude * Math.PI / 180.0)) / (1 - Math.sin(latitude * Math.PI / 180.0)))
+						/ 2.0);
+	}
+
+	private double pixelSpaceXToLongitude(double pixelX) {
+		return ((Math.round(pixelX) - MERCATOR_OFFSET) / MERCATOR_RADIUS) * 180.0 / Math.PI;
+	}
+
+	private double pixelSpaceYToLatitude(double pixelY) {
+		return (Math.PI / 2.0 - 2.0 * Math.atan(Math.exp((Math.round(pixelY) - MERCATOR_OFFSET) / MERCATOR_RADIUS)))
+				* 180.0 / Math.PI;
+	}
+
 }
